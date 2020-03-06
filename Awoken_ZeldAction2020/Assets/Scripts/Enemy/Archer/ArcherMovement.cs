b1f2623ef12e69e30d3dfe1a71bfe43a66eee9c9
Leroux <a.leroux@ubika-edu.com>
2020-03-06 12:28:31 +0100

    Git reparation
    Start of Archer movement

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enemy/Archer/ArcherMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Made by Antoine
+/// This script involve movement of archer and facing direction 
+/// </summary>
+
+public class ArcherMovement : MonoBehaviour
+{
+    [SerializeField] [Min(0)]
+    private float speed;
+    public float retreatDistance;
+    public float chaseDistance;
+    public float attackDistance;
+
+    private GameObject player;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        player = PlayerManager.Instance.gameObject;
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);           //Calculate distance between archer && player
+        Debug.Log(distance);
+
+        Vector2 direction = (player.transform.position - transform.position).normalized;            //Calculate direction between archer && player
+
+        if (distance < chaseDistance && distance > attackDistance)                                  //Move to player
+        {
+            rb.velocity = direction * speed * Time.fixedDeltaTime;
+        }
+        else if (distance < retreatDistance)                                                        //Escape from player
+        {
+            rb.velocity = direction * -speed * Time.fixedDeltaTime;
+        }
+        else if (distance < attackDistance && distance > retreatDistance)                           //Stop at his position
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
