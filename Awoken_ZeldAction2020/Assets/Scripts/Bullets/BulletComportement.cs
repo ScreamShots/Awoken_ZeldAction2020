932858a7503ec650e04b871ce59cd78e6730687c
Leroux <a.leroux@rubika-edu.com>
2020-03-09 15:52:37 +0100

    Archer life && bullet dmg set

--- Awoken_ZeldAction2020/Assets/Scripts/Bullets/BulletComportement.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Bullets/BulletComportement.cs
@@ -12,24 +12,32 @@ public class BulletComportement : MonoBehaviour
     #region Variables
 
     [Header("Bullet Stats")]
+    [Min(0)]
+    [SerializeField] private float dmg;
+
     public float bulletSpeed;
 
     public float timeBtwShots;
     
     [Space]
     public float TimeBeforeDestroy;
+
+    private GameObject player;
     #endregion
 
     private void Start()
     {
         Destroy(gameObject, TimeBeforeDestroy);                 //Destroy bullet after x time : security
+
+        player = PlayerManager.Instance.gameObject;
     }
 
     private void OnTriggerEnter2D(Collider2D other)             //When collide with player destroy bullet
     {
-        if (other.CompareTag("HitBox"))
+        if (other.CompareTag("HitBox") && other.gameObject.transform.root.CompareTag("Player"))
         {
             Destroy(gameObject);
+            player.GetComponent<BasicHealthSystem>().TakeDmg(dmg);
         }
     }
 }
