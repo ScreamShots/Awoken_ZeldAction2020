2e0d8b687072afffbbe06faba55fd0f9e47216b5
Leroux <a.leroux@ubika-edu.com>
2020-03-16 16:32:08 +0100

    Random movement of Poulion (state 1)

--- Awoken_ZeldAction2020/Assets/Scripts/Enemy/Poulion/PoulionMovement.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enemy/Poulion/PoulionMovement.cs
@@ -5,24 +5,33 @@ using DevTools;
 
 /// <summary>
 /// Made by Antoine
-/// This script involve movement of archer and facing direction 
+/// This script gather movement of Poulion and facing direction
 /// </summary>
 
-public class ArcherMovement : MonoBehaviour
+public class PoulionMovement : MonoBehaviour
 {
     #region Variables
-    [Header("Archer Speed")]
+    [Header("Poulion Speed")]
 
-    [SerializeField] [Min(0)]
+    [SerializeField]
+    [Min(0)]
+    private float neutralSpeed = 0;
+
+    [SerializeField]
+    [Min(0)]
     private float chaseSpeed = 0;
 
-    [SerializeField] [Min(0)]
-    private float retreatSpeed = 0;
+    [SerializeField]
+    [Min(0)]
+    private float attackSpeed = 0;
 
-    [Header("Archer Movement")]
-    public float retreatDistance;
+    [Header("Poulion Movement")]
     public float chaseDistance;
     public float attackDistance;
+    public float timeChangingDirection;
+    public float timeBeforePause;
+
+    private float timeLeft;
 
     private GameObject player;
 
@@ -41,11 +50,11 @@ public class ArcherMovement : MonoBehaviour
         #region Activate / Desactivate range circles on tick the var bool 
         if (showRanges == true && areRangesDisplayed == false)
         {
-            if(allRangesCircles != null)
+            if (allRangesCircles != null)
             {
                 allRangesCircles.SetActive(true);
                 areRangesDisplayed = true;
-            }           
+            }
         }
         else if (showRanges == false && areRangesDisplayed == true)
         {
@@ -59,48 +68,46 @@ public class ArcherMovement : MonoBehaviour
         #endregion
     }
 
-
     private void Start()
     {
         player = PlayerManager.Instance.gameObject;
 
         rb = gameObject.GetComponent<Rigidbody2D>();
 
+        timeLeft = timeChangingDirection;
+
         DrawRangeCircles();
     }
 
     private void FixedUpdate()
     {
-        Move();  
+        Move();
     }
 
     void Move()
-    {                                                
-        float distance = Vector2.Distance(transform.position, player.transform.position);                                           //Calculate distance between archer && player
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);                                           //Calculate distance between poulion && player
 
 
-        Vector2 direction = (player.transform.position - transform.position).normalized;                                                //Calculate direction between archer && player
+        Vector2 direction = (player.transform.position - transform.position).normalized;                                                //Calculate direction between poulion && player
 
 
-        if (distance <= chaseDistance && distance > attackDistance && !GetComponent<ArcherAttack>().archerIsAttacking)                   //Move to player if archer isn't attack
+        if (distance > chaseDistance)                                                                                                   //Random movement 
         {
-            rb.velocity = direction * chaseSpeed * Time.fixedDeltaTime;
-            GetComponent<ArcherAttack>().archerCanAttack = false;
+            timeLeft -= Time.fixedDeltaTime;
+
+            if(timeLeft <= 0)
+            {
+                StartCoroutine(RandomMove());
+            }
         }
-        else if (distance <= retreatDistance && !gameObject.GetComponent<ArcherAttack>().archerIsAttacking)                              //Escape from player if archer isn't attack
-        {
-            rb.velocity = direction * -retreatSpeed * Time.fixedDeltaTime;
-            GetComponent<ArcherAttack>().archerCanAttack = false;
-        }
-        else if (distance < chaseDistance && distance > retreatDistance)                                                                //Stop at his position
+        else if (distance > attackDistance && distance < chaseDistance)                                                                 //Move to player in zigzagging
         {
             rb.velocity = Vector2.zero;
-            GetComponent<ArcherAttack>().archerCanAttack = true;
         }
-        else if(distance > chaseDistance)
+        else if (distance < attackDistance)                                                                                             //Attack of the poulion
         {
             rb.velocity = Vector2.zero;
-            GetComponent<ArcherAttack>().archerCanAttack = false;
         }
     }
 
@@ -112,35 +119,39 @@ public class ArcherMovement : MonoBehaviour
         allRangesCircles.transform.parent = transform;
         allRangesCircles.transform.localPosition = new Vector3(0, 0, 0);
 
-        var retreatRangeDisplay = new GameObject { name = "Retreat Circle" };
-        retreatRangeDisplay.transform.parent = allRangesCircles.transform;        
-
         var chaseRangeDisplay = new GameObject { name = "Chase Circle" };
-        chaseRangeDisplay.transform.parent = allRangesCircles.transform;        
+        chaseRangeDisplay.transform.parent = allRangesCircles.transform;
 
         var attackRangeDisplay = new GameObject { name = "Chase Circle" };
         attackRangeDisplay.transform.parent = allRangesCircles.transform;
-        
 
-        retreatRangeDisplay.DrawCircle(retreatDistance, 0.02f, 50, Color.green);
         chaseRangeDisplay.DrawCircle(chaseDistance, 0.02f, 50, Color.yellow);
         attackRangeDisplay.DrawCircle(attackDistance, 0.02f, 50, Color.red);
 
         attackRangeDisplay.transform.localPosition = new Vector3(0, 0, 0);
         chaseRangeDisplay.transform.localPosition = new Vector3(0, 0, 0);
-        retreatRangeDisplay.transform.localPosition = new Vector3(0, 0, 0);
 
         allRangesCircles.SetActive(false);
-    }                       //function that draw 3 circle, one for eache range the archer has (attack, chase and retreat)
+    }                       //function that draw 2 circle, one for eache range the poulion has (attack and chase)
 
     [ContextMenu("Refresh Range Circles")]
     void RefreshRangeCircles()
     {
-        if(allRangesCircles != null)
+        if (allRangesCircles != null)
         {
             DestroyImmediate(allRangesCircles);
-        }        
+        }
         DrawRangeCircles();
         showRanges = false;
-    }                   //function that refresh the range circles by calling this function from the inspector (right click on the name of the script)
+    }                       //function that refresh the range circles by calling this function from the inspector (right click on the name of the script)
+
+    IEnumerator RandomMove()
+    {
+        timeLeft += timeChangingDirection;
+        Vector2 randomDirection = new Vector2(Random.Range(-10, +10), Random.Range(-10, +10));
+        rb.velocity = randomDirection.normalized * neutralSpeed * Time.fixedDeltaTime;
+
+        yield return new WaitForSeconds(timeBeforePause);
+        rb.velocity = Vector2.zero;
+    }
 }
