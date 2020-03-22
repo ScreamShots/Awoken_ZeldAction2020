60635992f45319d48e0a5a184dca92d64de33729
Bastien <B.prigent@rubika-edu.com>
2020-03-22 19:15:10 +0100

    Enigma1 Prog done
    Manque plus que l'intégration des graph et la bonne mise en place de l'énigme

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/DoorBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script made by Bastien Prigent
+/// This script Open and Closes differents enigma door
+/// </summary>
+public class DoorBehavior : MonoBehaviour
+{
+    public bool isDoorOpen;
+    [SerializeField]
+    private BoxCollider2D doorCollider;
+
+
+    void Start()
+    {
+        doorCollider = GetComponent<BoxCollider2D>();
+    }
+    void Update()
+    {
+        DoorIsClosed();
+        DoorIsOpen();
+    }
+    void DoorIsClosed()
+    {
+        if (isDoorOpen == false)
+        {
+            doorCollider.enabled = true;
+        }
+    }
+
+    void DoorIsOpen()
+    {
+        if (isDoorOpen == true)
+        {
+            doorCollider.enabled = false;
+        }
+    }
+}
