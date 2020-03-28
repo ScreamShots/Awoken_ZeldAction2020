5621f2542f352b52729755be0bb2179eddadc63e
Bastien <B.prigent@rubika-edu.com>
2020-03-28 17:09:30 +0100

    Dialogue n°1 with first PNJ
    GO LOOK FOR SOME VEGETABLES

---
+++ Awoken_ZeldAction2020/Assets/Scripts/PNJ/PNJ_01_Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PNJ_01_Dialogue : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pressX;
+    [SerializeField]
+    private GameObject dialogue01;
+    private bool playerIsHere;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "CollisionDetection" && other.transform.root.tag == "Player")
+        {
+            pressX.SetActive(true);
+            playerIsHere = true;
+        }
+    }
+
+    void Update()
+    {
+        if (playerIsHere == true && Input.GetButtonDown("Interraction") && dialogue01.activeInHierarchy == false)
+        {
+            dialogue01.SetActive(true);
+        }
+       else if (playerIsHere == true && Input.GetButtonDown("Interraction") && dialogue01.activeInHierarchy == true)
+       {
+            dialogue01.SetActive(false);
+       }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "CollisionDetection" && other.transform.root.tag == "Player")
+        {
+            pressX.SetActive(false);
+            dialogue01.SetActive(false);
+            playerIsHere = false;
+        }
+    }
+}
