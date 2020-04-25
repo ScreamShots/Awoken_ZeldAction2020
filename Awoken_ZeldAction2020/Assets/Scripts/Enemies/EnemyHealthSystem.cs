58145944b1b4429c6d0d5eb27af60fb4d3103f50
Leroux <a.leroux@ubika-edu.com>
2020-04-25 23:27:08 +0200

    Drop System on script enemy health system

--- Awoken_ZeldAction2020/Assets/Scripts/Enemies/EnemyHealthSystem.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enemies/EnemyHealthSystem.cs
@@ -27,6 +27,25 @@ public class EnemyHealthSystem : BasicHealthSystem
     bool canFlash = false;
     bool canFadeFlash = false;
 
+    #region Variables
+    private int itemNum;
+
+    private bool alreadyDropItem;
+    #endregion
+
+    #region Inspector Settings
+    [Header("Drop Position")]
+    [SerializeField] private float minDropDistance = 0;
+    [SerializeField] private float maxDropDistance = 0;
+
+    [Header("% Drop Chance %")]
+    public float dropChanceItem0;
+    public float dropChanceItem1;
+    public float dropChanceItem2;
+
+    [Space] public GameObject[] DropItemList;
+    #endregion
+
     /// <summary>
     /// The rest of the Health system behaviour (like maxHp, currentHp or TakeDmg() method are inherited from the basic system class
     /// Even if they are not visible here they are enable and u can call and use them
@@ -68,6 +87,7 @@ public class EnemyHealthSystem : BasicHealthSystem
     {
         Instantiate(corps, transform.position, Quaternion.identity);        //Instanciate a corps before destroy the object
         Destroy(gameObject);
+        DropItem();                                                   //When enemy PV <= 0, enemy is Destroy so call this function
     }
 
     void HitFlash()
@@ -96,6 +116,38 @@ public class EnemyHealthSystem : BasicHealthSystem
         {
             flashFadeTimer = flashFadeTime;
             canFadeFlash = false;
+        }
+    }
+
+    private void DropItem()
+    {
+        float randomNum = Random.Range(0, 101);                       //100% for determining loot chance
+        float randomNum1 = Random.Range(0, 101);
+        float randomNum2 = Random.Range(0, 101);
+
+        if (randomNum <= dropChanceItem0 & !alreadyDropItem)           //if a random number is inferior to % of dropping item we set in Inspector
+        {
+            itemNum = 0;
+            alreadyDropItem = true;                                   //can't drop 2 items on same enemy
+
+            GameObject itemDrop = Instantiate(DropItemList[itemNum], transform.position, Quaternion.identity);
+            itemDrop.transform.position = new Vector2(transform.position.x + Random.Range(minDropDistance, maxDropDistance), transform.position.y + Random.Range(minDropDistance, maxDropDistance));
+        }
+        if (randomNum1 <= dropChanceItem1 & !alreadyDropItem)
+        {
+            itemNum = 1;
+            alreadyDropItem = true;
+
+            GameObject itemDrop = Instantiate(DropItemList[itemNum], transform.position, Quaternion.identity);
+            itemDrop.transform.position = new Vector2(transform.position.x + Random.Range(minDropDistance, maxDropDistance), transform.position.y + Random.Range(minDropDistance, maxDropDistance));
+        }
+        if (randomNum2 <= dropChanceItem2 & !alreadyDropItem)
+        {
+            itemNum = 2;
+            alreadyDropItem = true;
+
+            GameObject itemDrop = Instantiate(DropItemList[itemNum], transform.position, Quaternion.identity);
+            itemDrop.transform.position = new Vector2(transform.position.x + Random.Range(minDropDistance, maxDropDistance), transform.position.y + Random.Range(minDropDistance, maxDropDistance));
         }
     }
 }
