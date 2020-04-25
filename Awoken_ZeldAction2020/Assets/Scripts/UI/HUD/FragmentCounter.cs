80f834f2f02e19dc73af9090eb91e2e324078519
Leroux <a.leroux@ubika-edu.com>
2020-04-25 18:34:18 +0200

    PickUps system & pickUp element finish (balancing required).

---
+++ Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/FragmentCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Made by Antoine
+/// This script is temporary. Use it to count the number of fragment Player have 
+/// </summary>
+
+public class FragmentCounter : MonoBehaviour
+{
+    #region SerialiazeFiled var Statement
+
+    [Header("Requiered Elements")]
+    [SerializeField] private Text fragmentNumber;
+
+    #endregion
+
+    private void Update()
+    {
+        fragmentNumber.text = "Fragment : " + PlayerManager.fragmentNumber;                //Show number of Fragments Player have
+    }
+}
