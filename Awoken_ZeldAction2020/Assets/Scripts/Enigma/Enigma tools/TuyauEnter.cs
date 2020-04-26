693bd1abf9dcb6cc4b1832721d3b87c3a9acf28e
Bastien <B.prigent@rubika-edu.com>
2020-04-26 15:55:57 +0200

    Tuyau coudé Enter/Exit done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauEnter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuyauEnter : MonoBehaviour
+{
+    public TuyauExit exit;
+    public GameObject bullet;
+
+
+    void Start()
+    {
+        exit = GetComponentInChildren<TuyauExit>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer ("PlayerProjectile"))
+        {
+            bullet = other.gameObject;
+            Debug.Log("Enter");
+            bullet.SetActive(false);
+            bullet.transform.position = exit.bulletTp.position;
+            bullet.SetActive(true);
+            bullet.GetComponent<BulletComportement>().aimDirection = exit.direction;
+        }
+    }
+}
