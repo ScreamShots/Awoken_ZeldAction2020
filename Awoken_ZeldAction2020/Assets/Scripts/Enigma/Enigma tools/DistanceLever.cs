f33ed3a8b67bbd789c7faaa07cc250b796a02533
Bastien <B.prigent@rubika-edu.com>
2020-04-26 18:02:26 +0200

    Intégration tuyau coudé

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/DistanceLever.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceLever : PressurePlateBehavior
+{
+    // Start is called before the first frame update
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.gameObject.layer == LayerMask.NameToLayer("PlayerProjectile")))
+        {
+            isPressed = true;
+            elementsOnPlate.Add(other.transform.root.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
