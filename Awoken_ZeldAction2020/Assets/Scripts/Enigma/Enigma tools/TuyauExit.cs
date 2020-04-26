7514ae01ec703c66a66c0ecb7319215d8c31f668
Bastien <B.prigent@rubika-edu.com>
2020-04-26 15:55:57 +0200

    Tuyau coudé Enter/Exit done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuyauExit : MonoBehaviour
+{
+
+    public Transform bulletTp;
+    public Vector2 direction;
+
+    void Start()
+    {
+        direction = bulletTp.transform.up;
+    }
+
+}
