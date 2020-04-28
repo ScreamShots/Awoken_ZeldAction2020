982b633e3a1af8be5f8b1713d279f6e45a61737c
Leroux <a.leroux@ubika-edu.com>
2020-04-28 10:34:01 +0200

    Instancie cloud on spawner then destroy

---
+++ Awoken_ZeldAction2020/Assets/Scripts/GameElements/Spawner/SpawnCloud.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Made by Antoine
+/// This script destroy game object after animation
+/// </summary>
+
+public class SpawnCloud : MonoBehaviour
+{
+    [SerializeField] private float timeToDestroy;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+}
