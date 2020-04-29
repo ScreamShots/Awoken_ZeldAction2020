ee4079abe789a5de8508b5ea7616a682a15e57b8
Rémi Sécher <remi.secher@outlok.fr>
2020-04-29 10:06:34 +0200

    Making Prefabs for the Camera System

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Camera and LvlFrame/AreaManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AreaManager : MonoBehaviour
+{
+    [HideInInspector]
+    public CinemachineVirtualCamera thisAreaCam;
+
+    private void Awake()
+    {
+        thisAreaCam = GetComponentInChildren<CinemachineVirtualCamera>();
+        thisAreaCam.gameObject.GetComponent<CinemachineConfiner>().m_BoundingShape2D = GetComponent<PolygonCollider2D>();
+        thisAreaCam.Follow = PlayerManager.Instance.gameObject.transform;
+        thisAreaCam.Priority = 0;
+        thisAreaCam.gameObject.SetActive(false);
+    }
+
+    public void InitializeFirstCam()
+    {
+        thisAreaCam.gameObject.SetActive(true);
+        thisAreaCam.Priority = 1;
+    }
+
+    public void ActivateCam()
+    {
+        thisAreaCam.gameObject.SetActive(true);
+        thisAreaCam.Priority = 1;
+    }
+
+    public void DesactivateCam()
+    {
+        thisAreaCam.gameObject.SetActive(false);
+        thisAreaCam.Priority = 0;
+    }
+}
