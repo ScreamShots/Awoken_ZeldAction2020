10f3271aceaefc9118999bc4b27a96e98d856c43
Bastien <B.prigent@rubika-edu.com>
2020-05-04 23:30:53 +0200

    gitflow-feature-stash: PRBA_Enigmes

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Olympe floor 1/OlympeFloor1Enigma1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OlympeFloor1Enigma1 : EnigmaTool
+{
+    [SerializeField]
+    private InstantPressurePlate instantPlate1;
+    [SerializeField]
+    private InstantPressurePlate instantPlate2;
+    [SerializeField]
+    private DoorBehavior door1;
+
+    protected override void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        OpenTheDoor();
+    }
+
+    void OpenTheDoor()
+    {
+        if (instantPlate1.isPressed == true && instantPlate2.isPressed == true)
+        {
+            door1.isDoorOpen = true;
+        }
+    }
+}
