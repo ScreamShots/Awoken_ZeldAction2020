b68b6fa29997b1a074a3648185ec9ffe43955b5f
Leroux <a.leroux@ubika-edu.com>
2020-05-04 16:17:44 +0200

    Boss fight camera working (temporary)

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Camera and LvlFrame/ArenaCam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// Made by Antoine
+/// This script is use to switch camera between Player camera and Boss fight camera
+/// </summary>
+
+public class ArenaCam : MonoBehaviour
+{
+    #region Variables
+    TransitionArena transitionArenaScript;
+
+    #endregion
+
+    #region Inspector Settings
+    public GameObject playerCam;
+    public GameObject bossFightCamera;
+    public GameObject groupComponer;
+
+    #endregion
+
+    void Start()
+    {
+        transitionArenaScript = GetComponentInChildren<TransitionArena>();
+    }
+
+    private void Update()
+    {
+        if (transitionArenaScript.playerInZone)
+        {
+            bossFightCamera.SetActive(true);
+            groupComponer.SetActive(true);
+
+            playerCam.SetActive(false);
+        }
+        else
+        {
+            bossFightCamera.SetActive(false);
+            groupComponer.SetActive(false);
+
+            playerCam.SetActive(true);
+        }
+    }
+}
