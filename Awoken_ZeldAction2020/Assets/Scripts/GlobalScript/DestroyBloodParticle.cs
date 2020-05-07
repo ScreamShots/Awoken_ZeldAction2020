00a88d638b4568933234a7c17259563041467cdc
Leroux <a.leroux@ubika-edu.com>
2020-05-07 16:55:10 +0200

    Blood particle on enemy + PJ

---
+++ Awoken_ZeldAction2020/Assets/Scripts/GlobalScript/DestroyBloodParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Made by Antoine
+/// This script is use to destroy blood particle after played
+/// </summary>
+
+public class DestroyBloodParticle : MonoBehaviour
+{
+    ParticleSystem particleSystemComponent;
+
+    void Start()
+    {
+        particleSystemComponent = GetComponent<ParticleSystem>();
+
+        Destroy(gameObject, particleSystemComponent.main.duration);
+    }
+}
