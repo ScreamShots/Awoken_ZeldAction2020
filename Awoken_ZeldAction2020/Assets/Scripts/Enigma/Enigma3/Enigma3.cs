b07cd5808831d28319a84087b5bc1371dd218130
Bastien <B.prigent@rubika-edu.com>
2020-05-07 15:34:17 +0200

    Starting région 3 Enigma

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma3/Enigma3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enigma3 : EnigmaTool
+{
+    [SerializeField]
+    private AreaManager autel;
+    [SerializeField]
+    private DoorBehavior door1;
+    protected override void Start()
+    {
+        door1.isDoorOpen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnigmaDone();
+        OpenTheDoor();
+    }
+    void EnigmaDone()
+    {
+        if(autel.allEnemyAreDead == true)
+        {
+            isEnigmaDone = true;
+        }
+    }
+
+    void OpenTheDoor()
+    {
+        if (isEnigmaDone == true)
+        {
+            door1.isDoorOpen = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //Looks if the Player enters the pressure plate
+    {
+        if (other.tag == "CollisionDetection" && other.transform.root.tag == "Player")
+        {
+            door1.isDoorOpen = false;
+        }
+    }
+}
