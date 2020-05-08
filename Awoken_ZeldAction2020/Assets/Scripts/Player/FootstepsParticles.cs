c86b9fcfe43d2f23f4b68e6f44f91378179ebfbd
Leroux <a.leroux@ubika-edu.com>
2020-05-08 22:46:51 +0200

    Foot particle on PJ (not modified on prefab now)

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Player/FootstepsParticles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Made by Antoine
+/// This script is use for emit particle on foot of Player
+/// </summary>
+
+public class FootstepsParticles : MonoBehaviour
+{
+    #region Variables
+    
+    public ParticleSystem footParticles;
+
+    PlayerMovement playerMovementScript;
+
+    #endregion
+
+    void Start()
+    {
+        playerMovementScript = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if (!playerMovementScript.isRunning)
+        {
+            CreateFootParticle();
+        }
+    }
+
+    void CreateFootParticle()
+    {
+        footParticles.Play();
+    }
+}
