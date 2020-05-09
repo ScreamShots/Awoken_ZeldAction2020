0daf264cf329e42d14046eb24966c00994bd342b
Bastien <B.prigent@rubika-edu.com>
2020-05-09 18:56:48 +0200

    AUtel prefab

---
+++ Awoken_ZeldAction2020/Assets/Scripts/GameElements/Autel/AutelAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutelAnim : MonoBehaviour
+{
+    private Animator anim;
+    private Autel autel;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        autel = GetComponentInParent<Autel>();
+    }
+
+
+    void Update()
+    {
+        if (autel.isAutelActivated == true)
+        {
+            anim.SetBool("isActivated", true);
+        }
+    }
+}
