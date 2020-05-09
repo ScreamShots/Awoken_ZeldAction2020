587b7b9637287ca8e082ed226954b77313407c2a
Bastien <B.prigent@rubika-edu.com>
2020-05-09 11:56:01 +0200

    ZeusBigor Created

---
+++ Awoken_ZeldAction2020/Assets/Scripts/GameElements/ZeusBigor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeusBigor : MonoBehaviour
+{
+    public bool isStatueActivated;
+    [SerializeField]
+    bool canPlayerActivateStatue;
+    private InterractionButton xButton;
+
+    void Start()
+    {
+        isStatueActivated = false;
+        xButton = GetComponentInChildren<InterractionButton>();
+    }
+    void Update()
+    {
+        if(canPlayerActivateStatue && Input.GetButtonDown("Interraction"))
+        {
+            isStatueActivated = true;
+        }
+
+        if(isStatueActivated)
+        {
+            xButton.HideButton();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CollisionDetection") && collision.transform.root.CompareTag("Player"))
+        {
+            canPlayerActivateStatue = true;
+            xButton.ShowButton();
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CollisionDetection") && collision.transform.root.CompareTag("Player"))
+        {
+            canPlayerActivateStatue = false;
+            xButton.HideButton();
+        }
+    }
+
+
+}
