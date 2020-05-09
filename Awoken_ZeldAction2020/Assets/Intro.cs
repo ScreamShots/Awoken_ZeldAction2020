3a0bac440941748b62dc637a516f374913322f90
Rémi Sécher <remi.secher@outlok.fr>
2020-05-09 20:48:36 +0200

    Intro

---
+++ Awoken_ZeldAction2020/Assets/Intro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject UI;
+    public GameObject Video;
+
+    private void Update()
+    {
+        if(Input.anyKeyDown && Video.activeInHierarchy)
+        {
+            Video.SetActive(false);
+            UI.SetActive(true);
+        }
+    }
+}
