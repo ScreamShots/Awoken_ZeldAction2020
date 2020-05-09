c8eb6063b71925edffdd15870bd0c8cfb4e121de
Bastien <B.prigent@rubika-edu.com>
2020-05-09 17:11:38 +0200

    Dialogue Olympe

---
+++ Awoken_ZeldAction2020/Assets/Scripts/PRBA_Script_Dialogue/Dial1Olympe1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial1Olympe1 : MonoBehaviour
+{
+    private DialogueTrigger trigger;
+    [SerializeField]
+    private Collider2D triggerZone;
+    void Start()
+    {
+        trigger = GetComponentInChildren<DialogueTrigger>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CollisionDetection") && collision.transform.root.CompareTag("Player"))
+        {
+            trigger.StartDialogue();
+            PlayerMovement.playerRgb.velocity = Vector2.zero;
+            triggerZone.enabled = false;
+        }
+    }
+
+}
