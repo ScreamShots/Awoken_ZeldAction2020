3f5d92aa5fb1478aec3f70ea79476eaa5728d958
Bastien <B.prigent@rubika-edu.com>
2020-05-09 11:56:01 +0200

    ZeusBigor Created

---
+++ Awoken_ZeldAction2020/Assets/Scripts/GameElements/ZeusBigorAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeusBigorAnim : MonoBehaviour
+{
+    private Animator anim;
+    private ZeusBigor statue;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        statue = GetComponentInParent<ZeusBigor>();
+    }
+
+
+    void Update()
+    {
+        if (statue.isStatueActivated == true)
+        {
+            anim.SetBool("isActivated", true);
+        }
+    }
+}
