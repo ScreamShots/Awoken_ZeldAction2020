35350b7dfd99ff9ee0524dbaf9136d79895f3d11
Leroux <a.leroux@ubika-edu.com>
2020-05-09 14:41:03 +0200

    Start of dialogue when PJ walk first time in Arenaa

---
+++ Awoken_ZeldAction2020/Assets/Ressources/Dialogue/FirstWallBoss/FirstWalkInBoss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstWalkInBoss : MonoBehaviour
+{
+    [SerializeField] DialogueTrigger thisTrigger;
+
+    public Transform BossPlaceFirstWalk;
+    public Transform BossPlaceArena;
+
+    private void Start()
+    {
+        thisTrigger.StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (DialogueManager.Instance.dialoguePhaseIndex == 1 && thisTrigger.dialogueStarted)
+        {
+            //BossManager.Instance.gameObject.transform.localScale =
+        }
+        if (thisTrigger.dialogueEnded)
+        {
+            
+        }
+
+    }
+}
