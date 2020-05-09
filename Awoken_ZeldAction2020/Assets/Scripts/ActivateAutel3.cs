8bce92f73da107d559de0c11c68fde932b2aa499
Leroux <a.leroux@ubika-edu.com>
2020-05-09 20:29:13 +0200

    altar region 3 done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/ActivateAutel3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAutel3 : MonoBehaviour
+{
+    public GameObject autel;
+
+    void Update()
+    {
+        if (autel.GetComponent<Autel>().isAutelActivated)
+        {
+            ProgressionManager.Instance.unlockCharge = true;
+        }
+    }
+}
