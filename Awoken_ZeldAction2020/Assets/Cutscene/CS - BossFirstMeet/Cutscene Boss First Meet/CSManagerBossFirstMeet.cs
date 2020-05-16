321ab82636499e2bd99949b28d5620ebf58b57ac
Leroux <a.leroux@ubika-edu.com>
2020-05-17 01:59:47 +0200

    Start Boss first meet cutscene

---
+++ Awoken_ZeldAction2020/Assets/Cutscene/CS - BossFirstMeet/Cutscene Boss First Meet/CSManagerBossFirstMeet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CSManagerBossFirstMeet : BasicCutSceneManager
+{
+    [Header("Zeus Reaveal Specific Elements")]
+    [SerializeField]
+    GameObject zeus = null;
+    [SerializeField]
+    GameObject lightning = null;
+    [SerializeField]
+    GameObject cutSceneUI = null;
+    [SerializeField]
+    GameObject cinemachine = null;
+    [SerializeField]
+    GameObject playerUI = null;
+
+    override public void EndOfCutScene()
+    {
+        zeus.SetActive(false);
+        lightning.SetActive(false);
+        cutSceneUI.SetActive(false);
+        cinemachine.SetActive(false);
+        playerUI.SetActive(true);
+
+        base.EndOfCutScene();
+    }
+}
