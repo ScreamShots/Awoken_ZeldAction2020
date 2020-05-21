78b45ce9ea726fff1219a69b1a3da4cc36a5d705
Bastien <B.prigent@rubika-edu.com>
2020-05-21 14:35:14 +0200

    Floor 1 done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Rework Olympe floor 1/Olympe1Room3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Olympe1Room3 : EnigmaTool
+{
+    [SerializeField]
+    private StayPressurePlate plate1 = null;
+    [SerializeField]
+    private StayPressurePlate plate2 = null;
+    [SerializeField]
+    private DoorBehavior door1 = null;
+
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnigmaDone();
+        OpenDoor();
+    }
+
+    void EnigmaDone()
+    {
+        if (plate1.isPressed == true && plate2.isPressed == true)
+        {
+            isEnigmaDone = true;
+        }
+    }
+
+    void OpenDoor()
+    {
+        if(isEnigmaDone == true)
+        {
+            door1.isDoorOpen = true;
+            plate1.isPressed = true;
+            plate2.isPressed = true;
+        }
+    }
+}
