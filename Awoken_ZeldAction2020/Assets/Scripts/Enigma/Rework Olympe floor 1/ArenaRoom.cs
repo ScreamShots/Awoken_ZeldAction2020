9ce1e787eb54bae4c4b898553319741ddc320eda
Bastien <B.prigent@rubika-edu.com>
2020-05-21 14:35:14 +0200

    Floor 1 done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Rework Olympe floor 1/ArenaRoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaRoom : EnigmaTool
+{
+    [SerializeField]
+    private AreaManager arena;
+    [SerializeField]
+    private DoorBehavior door1;
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnigmaDone();
+        OpenDoor();
+    }
+
+    void EnigmaDone()
+    {
+        if (arena.allEnemyAreDead == true)
+        {
+            isEnigmaDone = true;
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (isEnigmaDone == true)
+        {
+            door1.isDoorOpen = true;
+        }
+    }
+}
