050897ea58251c618818c4ab940e2b29eb1e60cd
Leroux <a.leroux@ubika-edu.com>
2020-05-23 19:05:32 +0200

    Modif on cutscene charge & parry (no movement)

---
+++ Awoken_ZeldAction2020/Assets/Cutscene/CS - Parry/Cutscene Parry/CSManagerParry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CSManagerParry : BasicCutSceneManager
+{
+    [Header("Specific Elements")]
+    [SerializeField]
+    GameObject cinemachineAltar = null;
+    [SerializeField]
+    GameObject playerUI = null;
+
+    override public void EndOfCutScene()
+    {
+        cinemachineAltar.SetActive(false);
+        playerUI.SetActive(true);
+
+        base.EndOfCutScene();
+    }
+
+    [ContextMenu("StartCutSceneParry")]
+    public override void StartCutScene()
+    {
+        cinemachineAltar.SetActive(true);
+        playerUI.SetActive(false);
+
+        base.StartCutScene();
+    }
+}
