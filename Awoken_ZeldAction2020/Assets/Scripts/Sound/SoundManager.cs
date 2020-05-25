5325c657f52b35925e2f612c361753036abbdb61
Leroux <a.leroux@ubika-edu.com>
2020-05-25 19:13:46 +0200

    Start of new sound system

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Made by Antoine
+/// New version of Sound Manager
+/// </summary>
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+// = = = [ VARIABLES DEFINITION ] = = =
+
+    #region Inspector Settings
+    [Space]
+    [Header("Global")]
+    [Range(0f, 1f)] public float globalDefaultVolume = 0.5f;
+
+    [Space]
+    [Header("Musics")]
+    [Range(0f, 1f)] public float musicDefaultVolume = 0.5f;
+
+    [Space]
+    [Header("Ambiances")]
+    [Range(0f, 1f)] public float ambiancesDefaultVolume = 0.5f;
+
+    [Space]
+    [Header("SFX")]
+    [Range(0f, 1f)] public float sfxDefaultVolume = 0.5f;
+
+    [Space]
+    [Header("References")]
+    public AudioSource musicSource;
+    public AudioSource ambianceSource;
+    public AudioSource sfxSource;
+    #endregion
+
+// = = =
+
+// = = = [ MONOBEHAVIOR METHODS ] = = =
+
+    void Awake()
+    {
+        #region Make Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        #endregion
+    }
+
+// = = =
+
+// = = = [ CLASS METHODS ] = = =
+
+    /// <summary>
+    /// Start playing a given music.
+    /// </summary>
+    public void PlayMusic(AudioClip music, float volume = 1f)
+    {
+        musicSource.clip = music;
+        musicSource.volume = (musicDefaultVolume * volume) * globalDefaultVolume;
+
+        musicSource.Play();
+
+        return;
+    }
+
+    /// <summary>
+    /// Plays a given sfx. Specific volume and pitch can be specified in parameters.
+    /// </summary>
+    public void PlaySfx(AudioClip sfx, float volume = 1f, float pitch = 1f)
+    {
+        sfxSource.pitch = pitch;
+        sfxSource.PlayOneShot(sfx, (sfxDefaultVolume * volume) * globalDefaultVolume);
+
+        sfxSource.pitch = 1;
+
+        return;
+    }
+
+    public void PlaySfx3D(AudioSource sfx3D, float volume = 1f, float pitch = 1f)
+    {
+        sfx3D.pitch = pitch;
+        sfx3D.PlayOneShot(sfx3D.clip, (sfxDefaultVolume * volume) * globalDefaultVolume);
+
+        sfx3D.pitch = 1;
+
+        return;
+    }
+
+    // = = =
+}
---
+++ Awoken_ZeldAction2020/Assets/Scripts/Sound/TurretSound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class TurretSound : MonoBehaviour
+{
+    #region Variables
+    [Space]
+    [Header("Shooting Sound")]
+    public AudioClip turretShoot;
+    public AudioSource turretShootSource;
+    [Range(0f, 1f)] public float turretShootVolume = 0.5f;
+
+    private GameElementsHealthSystem turretHealthScript;
+    private TurretShoot turretShootScript;
+
+    private bool turretIsShooting = false;
+    #endregion
+
+    void Start()
+    {
+        turretShootScript = GetComponentInParent<TurretShoot>();
+        turretHealthScript = GetComponentInParent<GameElementsHealthSystem>();
+    }
+
+    void Update()
+    {
+        TurretShoot();
+    }
+
+    void TurretShoot()
+    {
+        if (turretShootScript.isShooting)
+        {
+            if (!turretIsShooting)
+            {
+                turretIsShooting = true;
+                //SoundManager.Instance.PlaySfx(turretShoot, turretShootVolume);
+                SoundManager.Instance.PlaySfx3D(turretShootSource, turretShootVolume);
+                //turretShootSource.PlayOneShot(turretShootSource.clip);
+            }
+        }
+        else
+        {
+            turretIsShooting = false;
+        }
+    }
+}
