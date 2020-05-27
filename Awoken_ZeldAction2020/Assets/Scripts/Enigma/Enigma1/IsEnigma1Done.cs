21242f31baa0edc26eb6ff59aecebef48eed965b
Leroux <a.leroux@ubika-edu.com>
2020-05-27 15:19:15 +0200

    AUDIO : pickUps sound + enigma 1 R1 script modif for vegetable sound

--- Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma1/IsEnigma1Done.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma1/IsEnigma1Done.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class IsEnigma1Done : EnigmaTool
 {
+    [Space]
+    [Header("Engima Sound")]
+    public AudioClip partialResolve;
+    [Range(0f, 1f)] public float partialResolveVolume = 0.5f;
+
+    public AudioClip globalResolve;
+    [Range(0f, 1f)] public float globalResolveVolume = 0.5f;
+
+    private bool firstVegetable = false;
+    private bool secondVegetable = false;
+    private bool thirdVegetable = false;
 
     public static int nbrOfFoodCollected;
     static IsEnigma1Done enigmaInstance;
@@ -15,10 +27,25 @@ public class IsEnigma1Done : EnigmaTool
 
     public static void CollectFood()
     {
+        if (FoodPickUp.nbrOfFood == 1 && !firstVegetable)
         nbrOfFoodCollected += 1;
 
         if (nbrOfFoodCollected == 3)
         {
+            firstVegetable = true;
+            SoundManager.Instance.PlaySfx(partialResolve, partialResolveVolume);
+        }
+        else if (FoodPickUp.nbrOfFood == 2 && !secondVegetable)
+        {
+            secondVegetable = true;
+            SoundManager.Instance.PlaySfx(partialResolve, partialResolveVolume);
+        }
+        else if (FoodPickUp.nbrOfFood == 3 && !thirdVegetable)
+        {
+            isEnigmaDone = true;
+
+            thirdVegetable = true;
+            SoundManager.Instance.PlaySfx(globalResolve, globalResolveVolume);
             enigmaInstance.isEnigmaDone = true;
             ProgressionManager.Instance.thisSessionTimeLine = ProgressionManager.ProgressionTimeLine.VegetablesEnd;
         }
