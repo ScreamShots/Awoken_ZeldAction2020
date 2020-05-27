fe6d948b813dd6186f434ef3869b58b90a2c3de3
Leroux <a.leroux@ubika-edu.com>
2020-05-27 19:36:43 +0200

    AUDIO : Boss sound done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Sound/Enemy/ZeusSound.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ZeusSound : MonoBehaviour
+{
+    #region Variables
+    [Space]
+    [Header("Damage Sound")]
+    public AudioClip[] zeusDamage;
+    [Range(0f, 1f)] public float zeusDamageVolume = 0.5f;
+
+    BossManager bossManagerScript;
+
+    private bool zeusTakingDmg = false;
+
+    #endregion
+
+    void Start()
+    {
+        bossManagerScript = GetComponentInParent<BossManager>();
+    }
+
+    void Update()
+    {
+        ZeusTakeDamage();
+    }
+
+    void ZeusTakeDamage()
+    {
+        if (bossManagerScript.canFlash)
+        {
+            if (!zeusTakingDmg)
+            {
+                zeusTakingDmg = true;
+                SoundManager.Instance.PlayRandomSfx(zeusDamage, zeusDamageVolume);
+            }
+        }
+        else
+        {
+            zeusTakingDmg = false;
+        }
+    }
+}
