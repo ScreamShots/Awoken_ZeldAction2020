e5a038f29985e9a0b6bd9e52fbdcaea62dee953f
Leroux <a.leroux@ubika-edu.com>
2020-05-29 15:44:42 +0200

    Enigma parry transi cam done

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma2/EnigmaParryCam.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnigmaParryCam : MonoBehaviour
+{
+    #region Variables
+    public Enigma2 enigma2Script;
+    ProjectileDetectionZone projectileDetectionZoneScript;
+    TurretShoot turretShootScript;
+
+    private bool isBlending = false;
+
+    #endregion
+
+    #region Inspector Settings
+    public GameObject parryTurret = null;
+
+    [Header("Cams")]
+    [Space] public GameObject camArena3 = null;
+    public GameObject globalArenaCam = null;
+
+    [Header("Time")]
+    public float timeStayInGlobalCam = 0;
+
+    #endregion
+
+    void Start()
+    {
+        projectileDetectionZoneScript = GetComponentInChildren<ProjectileDetectionZone>();
+        turretShootScript = parryTurret.GetComponent<TurretShoot>();
+        globalArenaCam.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (projectileDetectionZoneScript.bulletDetection && !isBlending)                           //If bullet pass the first room, start the camera transition
+        {
+            isBlending = true;
+            turretShootScript.isActivated = false;
+            StartCoroutine(DeZoomArena());
+        }
+    }
+
+    IEnumerator DeZoomArena()
+    {
+        camArena3.SetActive(false);
+        globalArenaCam.SetActive(true);
+
+        yield return new WaitForSeconds(timeStayInGlobalCam);
+
+        camArena3.SetActive(true);
+        globalArenaCam.SetActive(false);
+        isBlending = false;
+        if (!enigma2Script.distanceLever1.isPressed)
+        {
+            turretShootScript.isActivated = true;
+        }
+    }
+}
