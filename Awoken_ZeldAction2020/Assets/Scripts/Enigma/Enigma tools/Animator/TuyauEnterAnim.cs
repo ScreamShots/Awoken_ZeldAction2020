8c0bd9f3442275f67aac4232a5b41905462dbcf1
Bastien <B.prigent@rubika-edu.com>
2020-05-29 18:45:37 +0200

    Tuyau Enter/exit animation done + vase cassable

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/Animator/TuyauEnterAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuyauEnterAnim : MonoBehaviour
+{
+    private TuyauEnter enter;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enter = GetComponentInParent<TuyauEnter>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayAnim();
+    }
+
+    void PlayAnim()
+    {
+        if (enter.isEnter == true)
+        {
+            anim.SetBool("isEnter", true);
+        }
+        else if (enter.isEnter == false)
+        {
+            anim.SetBool("isEnter", false);
+        }
+    }
+}
--- Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauEnter.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauEnter.cs
@@ -6,19 +6,26 @@ public class TuyauEnter : MonoBehaviour
 {
     public Transform exit;
     public GameObject bullet;
+    [Header("Pipe Duration")]
     [SerializeField]
-    public float bulletTimeTravel = 0;
-    [HideInInspector] public bool bulletIsEnter = false;
-    [HideInInspector] public bool bulletInsideZone = false;
+    private float bulletTimeTravel = 0;
+    [SerializeField]
+    private float nbrOfPipes;
+    private float animDuration;
+    [HideInInspector]
+    public bool isEnter;
+    [HideInInspector]
+    public bool isExit;
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer ("PlayerProjectile"))
         {
+            StartCoroutine(animEnter());
             bullet = other.gameObject;
             StartCoroutine(BulletTrajectory());
-            bulletInsideZone = true;
+
         }
         else
         {
@@ -31,10 +38,25 @@ public class TuyauEnter : MonoBehaviour
         bulletIsEnter = true;
         bullet.SetActive(false);
         bullet.transform.position = exit.position;
-        yield return new WaitForSeconds(bulletTimeTravel);
+        yield return new WaitForSeconds(bulletTimeTravel * nbrOfPipes);
+        StartCoroutine(AnimExit());
         bullet.SetActive(true);
         bullet.GetComponent<BulletComportement>().aimDirection = exit.up;
         bulletIsEnter = false;
+    }
+
+    private IEnumerator animEnter()
+    {
+        isEnter = true;
+        yield return new WaitForSeconds(1f);
+        isEnter = false;
+    }
+
+    private IEnumerator AnimExit()
+    {
+        isExit = true;
+        yield return new WaitForSeconds(1f);
+        isExit = false;
     }
 
 }
