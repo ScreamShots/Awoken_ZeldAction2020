60eef4e2fb0c7444622df497606b964db410a059
Leroux <a.leroux@ubika-edu.com>
2020-05-29 17:08:06 +0200

    New region 1 sound + sound enigma 1& 2 done

--- Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma1/IsEnigma1Done.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma1/IsEnigma1Done.cs
@@ -5,7 +5,7 @@ using UnityEngine.Audio;
 
 public class IsEnigma1Done : EnigmaTool
 {
-    /*[Space]
+    [Space]
     [Header("Engima Sound")]
     public AudioClip partialResolve;
     [Range(0f, 1f)] public float partialResolveVolume = 0.5f;
@@ -15,7 +15,7 @@ public class IsEnigma1Done : EnigmaTool
 
     private bool firstVegetable = false;
     private bool secondVegetable = false;
-    private bool thirdVegetable = false;*/
+    private bool thirdVegetable = false;
 
     public static int nbrOfFoodCollected;
     static IsEnigma1Done enigmaInstance;
@@ -27,28 +27,27 @@ public class IsEnigma1Done : EnigmaTool
 
     public static void CollectFood()
     {
-        //if (FoodPickUp.nbrOfFood == 1 && !firstVegetable)
         nbrOfFoodCollected += 1;
 
         if (nbrOfFoodCollected == 3)
         {
-            //firstVegetable = true;
             enigmaInstance.isEnigmaDone = true;
-            //SoundManager.Instance.PlaySfx(partialResolve, partialResolveVolume);
         }
-        /*else if (FoodPickUp.nbrOfFood == 2 && !secondVegetable)
-        {
-            secondVegetable = true;
-            SoundManager.Instance.PlaySfx(partialResolve, partialResolveVolume);
-        }
-        else if (FoodPickUp.nbrOfFood == 3 && !thirdVegetable)
-        {
-            isEnigmaDone = true;
 
-            thirdVegetable = true;
-            SoundManager.Instance.PlaySfx(globalResolve, globalResolveVolume);
-            enigmaInstance.isEnigmaDone = true;
-            
-        }*/
+        if (nbrOfFoodCollected == 1 && !enigmaInstance.firstVegetable)
+        {
+            enigmaInstance.firstVegetable = true;
+            SoundManager.Instance.PlaySfx(enigmaInstance.partialResolve, enigmaInstance.partialResolveVolume);
+        }
+        else if (nbrOfFoodCollected == 2 && !enigmaInstance.secondVegetable)
+        {
+            enigmaInstance.secondVegetable = true;
+            SoundManager.Instance.PlaySfx(enigmaInstance.partialResolve, enigmaInstance.partialResolveVolume);
+        }
+        else if (nbrOfFoodCollected == 3 && !enigmaInstance.thirdVegetable)
+        {
+            enigmaInstance.thirdVegetable = true;
+            SoundManager.Instance.PlaySfx(enigmaInstance.globalResolve, enigmaInstance.globalResolveVolume);         
+        }
     }
 }
