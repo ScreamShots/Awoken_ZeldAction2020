a537b3d6fe08cb3c24d14cc8f63a63ef4175defd
Bastien <B.prigent@rubika-edu.com>
2020-05-29 18:45:37 +0200

    Tuyau Enter/exit animation done + vase cassable

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/Animator/TuyauExitAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuyauExitAnim : MonoBehaviour
+{
+    private TuyauEnter exit;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit = GetComponentInParent<TuyauEnter>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayExit();
+    }
+
+    void PlayExit()
+    {
+        if(exit.isExit == true)
+        {
+            anim.SetBool("isExit", true);
+        }
+        else if (exit.isExit == false)
+        {
+            anim.SetBool("isExit", false);
+        }
+    }
+}
--- Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauEnter.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/Enigma/Enigma tools/TuyauEnter.cs
@@ -6,16 +6,26 @@ public class TuyauEnter : MonoBehaviour
 {
     public Transform exit;
     public GameObject bullet;
+    [Header("Pipe Duration")]
     [SerializeField]
     private float bulletTimeTravel = 0;
+    [SerializeField]
+    private float nbrOfPipes;
+    private float animDuration;
+    [HideInInspector]
+    public bool isEnter;
+    [HideInInspector]
+    public bool isExit;
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer ("PlayerProjectile"))
         {
+            StartCoroutine(animEnter());
             bullet = other.gameObject;
             StartCoroutine(BulletTrajectory());
+
         }
     }
 
@@ -23,9 +33,24 @@ public class TuyauEnter : MonoBehaviour
     {
         bullet.SetActive(false);
         bullet.transform.position = exit.position;
-        yield return new WaitForSeconds(bulletTimeTravel);
+        yield return new WaitForSeconds(bulletTimeTravel * nbrOfPipes);
+        StartCoroutine(AnimExit());
         bullet.SetActive(true);
         bullet.GetComponent<BulletComportement>().aimDirection = exit.up;
+    }
+
+    private IEnumerator animEnter()
+    {
+        isEnter = true;
+        yield return new WaitForSeconds(1f);
+        isEnter = false;
+    }
+
+    private IEnumerator AnimExit()
+    {
+        isExit = true;
+        yield return new WaitForSeconds(1f);
+        isExit = false;
     }
 
 }
