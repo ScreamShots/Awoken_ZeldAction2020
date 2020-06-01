d3426df0a19f8ebcf531af136e918ddcd58744b4
Leroux <a.leroux@ubika-edu.com>
2020-06-01 15:52:59 +0200

    Movable turret pushing sound + shockWave new asset

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Sound/GameElement/MovableTurretSound.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MovableTurretSound : MonoBehaviour
+{
+    #region Variables
+    [Space]
+    [Header("Shooting Sound")]
+    public AudioClip turretShoot;
+    [Range(0f, 1f)] public float turretShootVolume = 0.5f;
+
+    [Space]
+    [Header("Turret Moving Sound")]
+    public AudioSource turretMoving;
+    [Range(0f, 1f)] public float turretMovingVolume = 0.5f;
+
+    [Space]
+    [Header("Destroy Sound")]
+    public AudioClip turretDestroy;
+    [Range(0f, 1f)] public float turretDestroyVolume = 0.5f;
+
+    private TurretShoot turretShootScript;
+    private GameElementsHealthSystem gameElementHealthSystemScript;
+    private CubeToPush cubeToPushScript;
+
+    private bool turretIsShooting = false;
+    #endregion
+
+    void Start()
+    {
+        turretShootScript = GetComponentInParent<TurretShoot>();
+        gameElementHealthSystemScript = GetComponentInParent<GameElementsHealthSystem>();
+        cubeToPushScript = GetComponentInParent<CubeToPush>();
+        gameElementHealthSystemScript.onDead.AddListener(Dead);
+    }
+
+    void Update()
+    {
+        TurretShoot();
+        TurretMove();
+    }
+
+    void TurretMove()
+    {
+        if (cubeToPushScript.playerPushing)
+        {
+            SoundManager.Instance.PlayCubePushed(turretMoving, turretMovingVolume);
+        }
+        else
+        {
+            SoundManager.Instance.StopCubePushed(turretMoving);
+        }
+    }
+
+    void TurretShoot()
+    {
+        if (turretShootScript.isShooting)
+        {
+            if (!turretIsShooting)
+            {
+                turretIsShooting = true;
+                SoundManager.Instance.PlaySfx(turretShoot, turretShootVolume);
+            }
+        }
+        else
+        {
+            turretIsShooting = false;
+        }
+    }
+
+    void Dead()
+    {
+        SoundManager.Instance.PlaySfx(turretDestroy, turretDestroyVolume);
+    }
+}
