09c3e845fbaf44d929c7220c3771ed094818a44a
Leroux <a.leroux@ubika-edu.com>
2020-06-06 22:46:09 +0200

    Debug problem on shockWave

---
+++ Awoken_ZeldAction2020/Assets/Scripts/Debug & Dev Tools/SpawnSomething.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Made by Antoine
+/// This script is use to spawn something each x seconds
+/// </summary>
+
+public class SpawnSomething : MonoBehaviour
+{
+    public GameObject objectToSpawn = null;
+    public float timeBtwSpawn = 0;
+    private float time = 0;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            Instantiate(objectToSpawn, transform.position, objectToSpawn.transform.rotation);
+
+            time = timeBtwSpawn;
+        }
+    }
+}
