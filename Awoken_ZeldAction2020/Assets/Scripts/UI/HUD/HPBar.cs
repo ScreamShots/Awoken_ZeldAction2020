3f761cf4ec8590672f548dacd92b798f797a244c
Leroux <a.leroux@ubika-edu.com>
2020-06-09 14:29:50 +0200

    UI / Animation feedback on heart

--- Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/HPBar.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/HPBar.cs
@@ -14,8 +14,12 @@ public class HPBar : MonoBehaviour
 
     private PlayerHealthSystem playerHpSystem;
 
-    private float maxNumOfHearts;
-    private float currentNumOfHearts;
+    public float maxNumOfHearts;
+    public float currentNumOfHearts;
+
+    private Sprite currentSprite = null;
+
+    private Vector2 normalSizeDelta;
     #endregion
 
     #region SerialiazeFiled var Statement
@@ -34,6 +38,8 @@ public class HPBar : MonoBehaviour
 
     private void Start()
     {
+        normalSizeDelta = hearts[0].rectTransform.sizeDelta;
+
         if(PlayerManager.Instance != null)
         {
             playerHpSystem = PlayerManager.Instance.gameObject.GetComponent<PlayerHealthSystem>();       // getting the player's health managment script
@@ -54,6 +60,8 @@ public class HPBar : MonoBehaviour
 
             for (int i = 0; i < hearts.Length; i++)
             {
+                currentSprite = hearts[i].sprite;
+
                 if (i < maxNumOfHearts)
                 {
                     hearts[i].enabled = true;                               //check the max PV of Player and convert to heart max 
@@ -63,23 +71,73 @@ public class HPBar : MonoBehaviour
                     hearts[i].enabled = false;
                 }
 
-                hearts[i].sprite = emptyHeart;
-
-                for (float c = 0; c < currentNumOfHearts; c += 0.5f)
+                if (i <= (currentNumOfHearts - 1))
                 {
-                    int b = Mathf.FloorToInt(c);
-                    int e = (int)c;
+                    if (currentSprite == halfHeart)                          //regen half to full
+                    {
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Heal_full_half");
+                        hearts[i].sprite = fullHeart;
 
-                    if (c != (float)b)                                      //if the current heart value is not a decimal
-                    {
-                        hearts[e].sprite = fullHeart;
+                        StartCoroutine(DisableAnimator(i, fullHeart));
                     }
-                    else
+                    else if (currentSprite == emptyHeart)                    //regen empty to full
                     {
-                        hearts[e].sprite = halfHeart;                       //if the current heart value is a decimal
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Heal_full");
+                        hearts[i].sprite = fullHeart;
+
+                        StartCoroutine(DisableAnimator(i, fullHeart));
+                    }                
+                }
+                else if (i <= (currentNumOfHearts - 1) + 0.5f)
+                {
+                    if (currentSprite == fullHeart)                          //damage full to half
+                    {
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Damage_full_half");
+                        hearts[i].sprite = halfHeart;
+
+                        StartCoroutine(DisableAnimator(i, halfHeart));
+                    }
+                    else if (currentSprite == emptyHeart)                    //regen empty to half
+                    {
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Heal_half");
+                        hearts[i].sprite = halfHeart;
+
+                        StartCoroutine(DisableAnimator(i, halfHeart));
                     }
                 }
+                else
+                {
+                    if (currentSprite == fullHeart)                          //damage full to empty
+                    {
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Damage_full");
+                        hearts[i].sprite = emptyHeart;
+
+                        StartCoroutine(DisableAnimator(i, emptyHeart));
+                    }
+                    else if (currentSprite == halfHeart)                    //damage half to empty
+                    {
+                        hearts[i].GetComponent<Animator>().enabled = true;
+                        hearts[i].GetComponent<Animator>().SetTrigger("Damage_half");
+                        hearts[i].sprite = emptyHeart;
+
+                        StartCoroutine(DisableAnimator(i, emptyHeart));
+                    }
+                }              
             }
         }
+    }
+
+    IEnumerator DisableAnimator(int i, Sprite spriteToActivate)
+    {
+        yield return new WaitForSeconds(0.4f);
+
+        hearts[i].GetComponent<Animator>().enabled = false;
+        hearts[i].rectTransform.sizeDelta = normalSizeDelta;
+        hearts[i].sprite = spriteToActivate;
     }
 }
