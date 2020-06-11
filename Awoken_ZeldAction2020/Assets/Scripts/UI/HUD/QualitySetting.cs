782d20bacc933701a41747f262a5c7569eb9ee08
Antoine <a.leroux@rubika-edu.com>
2020-06-11 18:34:54 +0200

    main menu & pause menu finish

---
+++ Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/QualitySetting.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualitySetting : MonoBehaviour
+{
+    Dropdown dropdownScript = null;
+    Toggle toggleScript = null;
+
+    public enum enumType { Checkbox, Dropdown };
+    public enumType whichType;
+
+    void Start()
+    {
+        if (whichType == enumType.Checkbox)
+        {
+            toggleScript = GetComponent<Toggle>();
+            toggleScript.isOn = GameManager.Instance.gameFullscren;
+        }
+        else if (whichType == enumType.Dropdown)
+        {
+            dropdownScript = GetComponent<Dropdown>();
+            dropdownScript.value = GameManager.Instance.gameQuality;
+        }
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+        GameManager.Instance.gameFullscren = isFullScreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        if (qualityIndex == 0)
+        {
+            QualitySettings.SetQualityLevel(0);
+            GameManager.Instance.gameQuality = 0;
+        }
+        else if (qualityIndex == 1)
+        {
+            QualitySettings.SetQualityLevel(2);
+            GameManager.Instance.gameQuality = 1;
+        }
+        else if (qualityIndex == 2)
+        {
+            QualitySettings.SetQualityLevel(5);
+            GameManager.Instance.gameQuality = 2;
+        }
+    }
+}
--- Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/ShowValueSlider.cs
+++ Awoken_ZeldAction2020/Assets/Scripts/UI/HUD/ShowValueSlider.cs
@@ -7,10 +7,30 @@ using TMPro;
 public class ShowValueSlider : MonoBehaviour
 {
     private TextMeshProUGUI percentageText = null;
+    Slider sliderScript = null;
+
+    public enum enumVolume { Sfx, Music, Voice };
+    public enumVolume whichVolume;
 
     void Start()
     {
         percentageText = GetComponent<TextMeshProUGUI>();
+
+        sliderScript = GetComponentInParent<Slider>();
+        percentageText.text = (sliderScript.value * 100) + "%";
+
+        if (whichVolume == enumVolume.Sfx)
+        {
+            sliderScript.value = SoundManager.Instance.sfxDefaultVolume;
+        }
+        else if (whichVolume == enumVolume.Music)
+        {
+            sliderScript.value = SoundManager.Instance.musicDefaultVolume;
+        }
+        else if (whichVolume == enumVolume.Voice)
+        {
+            sliderScript.value = SoundManager.Instance.voiceDefaultVolume;
+        }
     }
 
     public void textUpdate(float value)
