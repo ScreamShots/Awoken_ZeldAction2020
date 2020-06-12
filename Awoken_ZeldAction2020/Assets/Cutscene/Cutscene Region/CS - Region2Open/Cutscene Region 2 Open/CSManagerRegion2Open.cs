88f980b148dc264ccce60c4c714ee316752fc85a
Antoine <a.leroux@rubika-edu.com>
2020-06-12 16:38:32 +0200

    Region 1 cam to show region 2 & region 3 open after Olympe floor

---
+++ Awoken_ZeldAction2020/Assets/Cutscene/Cutscene Region/CS - Region2Open/Cutscene Region 2 Open/CSManagerRegion2Open.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CSManagerRegion2Open : BasicCutSceneManager
+{
+    [Header("Specific Elements")]
+    [SerializeField]
+    GameObject Cinemachine = null;
+
+    override public void EndOfCutScene()
+    {
+        Cinemachine.SetActive(false);
+
+        base.EndOfCutScene();
+    }
+
+    [ContextMenu("StartCutSceneRegion2Open")]
+    public override void StartCutScene()
+    {
+        Cinemachine.SetActive(true);
+
+        base.StartCutScene();
+    }
+}
